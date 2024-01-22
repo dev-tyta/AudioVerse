579e96a6ac3dfeaa20c73a3d85b113e2e85b5ffe
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-22 13:42:53 +0100

    Work on AudioUtils classes

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -7,4 +7,6 @@ class AudioUtils:
         self.suno = pipeline('text-to-speech', model='suno/bark-small')
         self.suno.set_sample_rate(16000)
 
-
+    def speak(self, text="Hello World!"):
+        response = self.suno(text)
+        return response["audio"]
