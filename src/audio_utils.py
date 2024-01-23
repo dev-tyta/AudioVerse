54f34b7a8f4a3815c664646cbbbefb36e2a05ec9
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-23 21:40:37 +0100

    Using scipy to test on the output of the TTS project

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -9,4 +9,5 @@ class AudioUtils:
 
     def speak(self, text="Hello World!"):
         response = self.suno(text)
+        response = scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
         return response
