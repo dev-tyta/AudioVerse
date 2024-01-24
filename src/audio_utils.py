8c68c2fddfb508b6cc12f783e2cf34b6d382da21
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-24 22:13:10 +0100

    JK

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -8,6 +8,7 @@ class AudioUtils:
         self.suno.set_sample_rate(16000)
 
     def speak(self, text="Hello World!"):
+        print("Model Loaded")
         response = self.suno(text)
         response = scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
         return response
