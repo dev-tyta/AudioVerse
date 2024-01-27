b46c58a4be51373eaaeeb9a92256bd76058a6018
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-27 11:57:59 +0100

    Let's play voices

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -12,3 +12,7 @@ class AudioUtils:
         response = self.suno(text)
         response = scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
         return response
+
+
+test_audio = AudioUtils()
+output = test_audio.speak(text="I'm working on trying out the scripted version of the project to work.")
