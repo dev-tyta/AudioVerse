1dde52a20f871223df4a4274a9bc607c55dac607
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-29 08:55:12 +0100

    Streamlit version of project

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import scipy.io.wavfile
 import streamlit as st
+from src.audio_utils import AudioUtils
+import io
 
+
+def main():
+    st.title("AudioVerse")
+
+    text_input = st.text_input("Enter a text to be transformed", "Hello World")
+    audio_utils = AudioUtils()
+
+    if text_input is not None:
+        if st.button("Speak"):
+            audio_out, sample_rate = audio_utils.speak(text_input)
+            audio_bytes = io.BytesIO()
+            scipy.io.wavfile.write(audio_bytes, rate=sample_rate, data=audio_out)
+            audio_bytes.seek(0)
+
+            st.audio(audio_bytes, format="audio/wav")
+
+
+if __name__ == "__main__":
+    main()
