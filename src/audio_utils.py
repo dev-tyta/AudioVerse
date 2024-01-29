fee3c282b56ff064007b5e2358dd4b3a62d21cbf
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-01-29 04:52:31 +0100

    Wahali oti su mi o

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -4,15 +4,15 @@ from transformers import pipeline
 
 class AudioUtils:
     def __init__(self):
-        self.suno = pipeline('text-to-speech', model='suno/bark-small')
+        self.suno = pipeline(task='text-to-speech', model='suno/bark')
         self.suno.set_sample_rate(16000)
 
     def speak(self, text="Hello World!"):
         print("Model Loaded")
         response = self.suno(text)
-        response = scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
-        return response
+        scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
+        return response["audio"], response["sampling_rate"]
 
 
 test_audio = AudioUtils()
-output = test_audio.speak(text="I'm working on trying out the scripted version of the project to work.")
+output_audio, output_rate = test_audio.speak(text="I'm working on trying out the scripted version of the project to work.")
