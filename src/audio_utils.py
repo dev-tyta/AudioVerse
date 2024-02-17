93b1332cb96c186e108f9e94148a5485d0c31220
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-02-17 17:49:45 +0100

    Working Model for AudioVerse text to speech "

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -1,13 +1,19 @@
-import scipy
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+huggingface = os.getenv("HUGGINGFACE")
 
 
 class AudioUtils:
     def __init__(self):
         self.API_URL = "https://api-inference.huggingface.co/models/suno/bark-small"
+        self.headers = {"Authorization": f"Bearer {huggingface}"}
 
     def query(self, payload):
-        response = requests.post(self.API_URL, headers=headers, json=payload)
+        response = requests.post(self.API_URL, headers=self.headers, json=payload)
         return response.content
 
     def speak(self, text="Hello World!"):
@@ -15,7 +21,6 @@ class AudioUtils:
         response = self.query({
             "inputs": text,
         })
-
         return response
 
 
