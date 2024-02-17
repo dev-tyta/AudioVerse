fd896a522cc1651d597583504f442353e39e26db
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-02-17 16:59:27 +0100

    Work on audio_utils

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -6,20 +6,20 @@ class AudioUtils:
     def __init__(self):
         self.API_URL = "https://api-inference.huggingface.co/models/suno/bark-small"
 
-    def query(payload):
-	response = requests.post(self.API_URL, headers=headers, json=payload)
-	return response.content
+    def query(self, payload):
+        response = requests.post(self.API_URL, headers=headers, json=payload)
+        return response.content
 
     def speak(self, text="Hello World!"):
         print("Model Loaded")
-        response = query({
-	"inputs": text,
-})
-        
-        scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
+        response = self.query({
+            "inputs": text,
+        })
+
         return response
 
 
 test_audio = AudioUtils()
-output_audio = test_audio.speak(
-                                        text = "I'm working on trying out the scripted version of the project to work.")
+output_audio = test_audio.speak(text="I'm working on trying out the scripted version of the project to work.")
+
+print(output_audio)
