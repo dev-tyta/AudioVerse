6990c835552aabf21d66685b6a44a32d2941e6ce
Testimony Adekoya <74291997+dev-tyta@users.noreply.github.com>
2024-02-17 12:41:25 +0100

    Test

--- src/audio_utils.py
+++ src/audio_utils.py
@@ -1,19 +1,25 @@
 import scipy
-from transformers import pipeline
+import requests
 
 
 class AudioUtils:
     def __init__(self):
-        self.suno = pipeline(task='text-to-speech', model='suno/bark')
-        self.suno.set_sample_rate(16000)
+        self.API_URL = "https://api-inference.huggingface.co/models/suno/bark-small"
+
+    def query(payload):
+	response = requests.post(self.API_URL, headers=headers, json=payload)
+	return response.content
 
     def speak(self, text="Hello World!"):
         print("Model Loaded")
-        response = self.suno(text)
+        response = query({
+	"inputs": text,
+})
+        
         scipy.io.wavfile.write("bark_out.wav", rate=response["sampling_rate"], data=response["audio"])
-        return response["audio"], response["sampling_rate"]
+        return response
 
 
 test_audio = AudioUtils()
-output_audio, output_rate = test_audio.speak(
-                                        text="I'm working on trying out the scripted version of the project to work.")
+output_audio = test_audio.speak(
+                                        text = "I'm working on trying out the scripted version of the project to work.")
